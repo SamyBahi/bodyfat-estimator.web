74e4be09fb121582963480a16188dac11b0286d6
Samy Bahi <samy@samybahi.com>
2023-09-01 16:52:43 +0200

    escaped chars

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,21 +11,21 @@ export default function About() {
           <p className="opacity-60 font-medium">
             This web app uses artificial intelligence to estimate your density
             taking your mensurations as an input, and computes your bodyfat
-            according to Siri's formula (1956).
+            according to Siri&apos;s formula (1956).
           </p>
           <p className="opacity-60 font-medium mt-6">
             Prior to the adoption of DXA, the most accurate method of estimating
-            body fat percentage was to measure that person's average density
-            (total mass divided by total volume) and apply a formula to convert
-            that to body fat percentage. Since fat tissue has a lower density
-            than muscles and bones, it is possible to estimate the fat content.
-            This estimate is distorted by the fact that muscles and bones have
-            different densities: for a person with a more-than-average amount of
-            bone mass, the estimate will be too low. However, this method gives
-            highly reproducible results for individual persons (± 1 to 3%). As
-            measuring body density can be hard and costly, I had the idea to
-            implement a machine learning model that is capable of predicting
-            density given mesnurations.
+            body fat percentage was to measure that person&apos;s average
+            density (total mass divided by total volume) and apply a formula to
+            convert that to body fat percentage. Since fat tissue has a lower
+            density than muscles and bones, it is possible to estimate the fat
+            content. This estimate is distorted by the fact that muscles and
+            bones have different densities: for a person with a
+            more-than-average amount of bone mass, the estimate will be too low.
+            However, this method gives highly reproducible results for
+            individual persons (± 1 to 3%). As measuring body density can be
+            hard and costly, I had the idea to implement a machine learning
+            model that is capable of predicting density given mesnurations.
           </p>
           <p className="opacity-60 font-medium mt-6">
             The model used for density prediction is a RandomForest Regressor.
@@ -39,7 +39,7 @@ export default function About() {
             >
               Kaggle
             </a>
-            . You can also find the notebook that led to the model's actual
+            . You can also find the notebook that led to the model&apos;s actual
             state on my github following this{" "}
             <a
               href="https://github.com/SamyBahi/bodyfat-estimation"
@@ -53,7 +53,7 @@ export default function About() {
           </p>
           <p className="opacity-60 font-medium my-6">
             Please note that the result may be off by around 1-3% according to
-            the fact that Siri's formula may also be off by around 1-3%.
+            the fact that Siri&apos;s formula may also be off by around 1-3%.
             However, this model should be more precise than bioelectrical
             impedance scale (3-5% off).
           </p>
@@ -63,11 +63,11 @@ export default function About() {
           <p className="opacity-60 font-medium">
             Bodyfat is the total amount of fat in your body. Everybody has at
             least a little bit of bodyfat. It is the extra, stored energy in
-            your body. It's like a savings account for calories. Just as you put
-            money in a savings account to use when you need it, your body stores
-            calories as fat to use for energy later. Having some body fat is
-            normal and healthy, but too much can cause health issues (contrarely
-            to money in your bank account).
+            your body. It&apos;s like a savings account for calories. Just as
+            you put money in a savings account to use when you need it, your
+            body stores calories as fat to use for energy later. Having some
+            body fat is normal and healthy, but too much can cause health issues
+            (contrarely to money in your bank account).
           </p>
           <p className="opacity-60 font-medium mt-6">
             Thus, predicting bodyfat percentage can be useful for many reasons,
