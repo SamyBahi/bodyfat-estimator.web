faaaae217683736df25bb3efb146b3dc489020e3
Samy Bahi <samy@samybahi.com>
2023-09-01 16:54:17 +0200

    fixed escape char

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
           <p className="opacity-60 font-medium">
             This web app uses artificial intelligence to estimate your density
             taking your mensurations as an input, and computes your bodyfat
-            according to Siri's formula (1956).
+            according to Siri&apos;s formula (1956).
           </p>
           <p className="opacity-60 font-medium my-6">
             To use this tool, just enter your mensurations in the form below and
             click Estimate to get your results. Please note that the result may
-            be off by around 1-3% according to the fact that Siri's formula may
-            also be off by around 1-3%.
+            be off by around 1-3% according to the fact that Siri&apos;s formula
+            may also be off by around 1-3%.
           </p>
         </div>
         <Providers>
